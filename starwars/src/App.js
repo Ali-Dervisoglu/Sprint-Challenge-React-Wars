581ec17b6938b2dc5f0c03b9d3b2987b218c9805
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-08-30 18:45:21 -0700

    totally stuck at rendering multiple elemenents. Used brute force.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App.scss';
 import axios from "axios";
 import CharacterCard from "./components/CharacterCard.js";
 
@@ -11,20 +11,52 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [character, setCharacter] = useState([]);
+  const [character1, setCharacter1] = useState([]);
+  const [character2, setCharacter2] = useState([]);
+  const [character3, setCharacter3] = useState([]);
+  const [character4, setCharacter4] = useState([]);
+  const [characters, setCharacters] = useState([]);
+
   useEffect(() => {
     axios.get('https://swapi.co/api/people/')
       .then(res => {
         console.log(res);
-        setCharacter(res.data.results[0]);
+        setCharacter1(res.data.results[0]);
+        setCharacter2(res.data.results[1]);
+        setCharacter3(res.data.results[2]);
+        setCharacter4(res.data.results[3]);
+        setCharacters(res.data.results)
       });
   }, []);
+
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <CharacterCard name={character.name}
-        birth_year={character.birth_year} />
+      <div class="character-card"><CharacterCard name={character1.name}
+        birth_year={character1.birth_year}
+        gender={character1.gender}
+        hair_color={character1.hair_color}
+        eye_color={character1.eye_color}
+        skin_color={character1.skin_color} /></div>
+      <div class="character-card"><CharacterCard name={character2.name}
+        birth_year={character2.birth_year}
+        gender={character2.gender}
+        hair_color={character2.hair_color}
+        eye_color={character2.eye_color}
+        skin_color={character2.skin_color} /></div>
+      <div class="character-card"><CharacterCard name={character3.name}
+        birth_year={character3.birth_year}
+        gender={character3.gender}
+        hair_color={character3.hair_color}
+        eye_color={character3.eye_color}
+        skin_color={character3.skin_color} /></div>
+        <div class="character-card"><CharacterCard name={character4.name}
+        birth_year={character4.birth_year}
+        gender={character4.gender}
+        hair_color={character4.hair_color}
+        eye_color={character4.eye_color}
+        skin_color={character4.skin_color} /></div>
     </div>
   );
 
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './CharacterCard.scss';
 
 function CharacterCard(props) {
     return (
         <div className="character-card">
-            <div>{props.name}</div>
-            <h2>birth year: {props.birth_year}</h2>
+            <h2>{props.name}</h2>
+            <div>birth year: {props.birth_year}</div>
+            <div>gender: {props.gender}</div>
+            <div>hair color: {props.hair_color}</div>
+            <div>eye color: {props.eye_color} </div>
+            <div>skin color: {props.skin_color}</div>
         </div>
     );
 }
